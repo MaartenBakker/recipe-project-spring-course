7763bb9c3ba2192b699ea2f97990838378d445a7
Maarten Bakker <maartenbakker@gmail.com>
2020-10-26 13:40:29 +0100

    Fix and cleanup on @Data @EqualsAndHashCode(exclude = ...) to prevent circular hashGeneration dependency

--- src/main/java/com/maartenmusic/recipeproject/domain/Category.java
+++ src/main/java/com/maartenmusic/recipeproject/domain/Category.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = "recipes")
 @NoArgsConstructor
 @Entity
 public class Category {
--- src/main/java/com/maartenmusic/recipeproject/domain/Recipe.java
+++ src/main/java/com/maartenmusic/recipeproject/domain/Recipe.java
@@ -8,7 +8,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
-@EqualsAndHashCode(exclude = {"ingredients", "notes", "categories"})
 @Entity
 public class Recipe {
 
