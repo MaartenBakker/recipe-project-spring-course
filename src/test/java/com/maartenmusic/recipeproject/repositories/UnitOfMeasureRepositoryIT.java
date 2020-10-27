4f7da1816c94bb11f39208c945d5e3b78f654f01
Maarten Bakker <maartenbakker@gmail.com>
2020-10-27 11:11:10 +0100

    Added an integration test

---
+++ src/test/java/com/maartenmusic/recipeproject/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,43 @@
+package com.maartenmusic.recipeproject.repositories;
+
+import com.maartenmusic.recipeproject.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() {
+
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+
+    }
+
+    @Test
+    public void findByDescriptionCup() {
+
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", unitOfMeasureOptional.get().getDescription());
+
+    }
+}
