c5e3fd04a33e0c02d5357367d43a7ea3151b175a
Maarten Bakker <maartenbakker@gmail.com>
2020-11-02 10:28:47 +0100

    Moved th:each from <ul> to <li> in show.html

--- src/test/java/com/maartenmusic/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/com/maartenmusic/recipeproject/controllers/IndexControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Mockito.*;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
