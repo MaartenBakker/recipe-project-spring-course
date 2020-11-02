9916de4274d7bdcae618dc06ac42c2d7a68ae109
MaartenBakker <maartenbakker@gmail.com>
2020-11-02 15:09:08 +0100

    Added converter Tests

--- src/test/java/com/maartenmusic/recipeproject/converters/CategoryCommandToCategoryTest.java
+++ src/test/java/com/maartenmusic/recipeproject/converters/CategoryCommandToCategoryTest.java
@@ -1,5 +1,7 @@
 package com.maartenmusic.recipeproject.converters;
 
+import com.maartenmusic.recipeproject.commands.CategoryCommand;
+import com.maartenmusic.recipeproject.domain.Category;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,11 +9,38 @@ import static org.junit.Assert.*;
 
 public class CategoryCommandToCategoryTest {
 
+    public static final Long ID_VALUE = 1L;
+    public static final String DESCRIPTION = "description";
+    CategoryCommandToCategory converter;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        converter = new CategoryCommandToCategory();
+    }
+
+    @Test
+    public void testNullObject() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() {
+        assertNotNull(converter.convert(new CategoryCommand()));
     }
 
     @Test
     public void convert() {
+        //given
+        CategoryCommand categoryCommand = new CategoryCommand();
+        categoryCommand.setId(ID_VALUE);
+        categoryCommand.setDescription(DESCRIPTION);
+
+        //when
+        Category category = converter.convert(categoryCommand);
+
+        //then
+        assertEquals(ID_VALUE, category.getId());
+        assertEquals(DESCRIPTION, category.getDescription());
+
     }
 }
