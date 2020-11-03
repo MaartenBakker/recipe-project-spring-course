0185d8a530564adc622ec5e15431a86236fa2ae6
Maarten Bakker <maartenbakker@gmail.com>
2020-11-03 16:56:01 +0100

    added Recipe form

--- src/main/java/com/maartenmusic/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/maartenmusic/recipeproject/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.maartenmusic.recipeproject.controllers;
 
+import com.maartenmusic.recipeproject.commands.RecipeCommand;
 import com.maartenmusic.recipeproject.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RecipeController {
@@ -15,10 +18,24 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @GetMapping("/recipe/show/{id}")
+    @GetMapping("recipe/show/{id}")
     public String showById(Model model, @PathVariable Long id) {
         model.addAttribute("recipe", recipeService.findById(id));
 
         return "recipe/show";
+    }
+
+    @GetMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
