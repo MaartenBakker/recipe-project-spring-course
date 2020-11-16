3ba93ff9799964304034070ad96af2ccd554528f
Maarten Bakker <maartenbakker@gmail.com>
2020-11-16 21:42:47 +0100

    Added NotFoundException and RecipeDBServiceTest getRecipeCommandByIdTest

---
+++ src/main/java/com/maartenmusic/recipeproject/exceptions/NotFoundException.java
@@ -0,0 +1,16 @@
+package com.maartenmusic.recipeproject.exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException() {
+        super();
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/test/java/com/maartenmusic/recipeproject/services/RecipeDBServiceTest.java
+++ src/test/java/com/maartenmusic/recipeproject/services/RecipeDBServiceTest.java
@@ -1,5 +1,6 @@
 package com.maartenmusic.recipeproject.services;
 
+import com.maartenmusic.recipeproject.commands.RecipeCommand;
 import com.maartenmusic.recipeproject.converters.RecipeCommandToRecipe;
 import com.maartenmusic.recipeproject.converters.RecipeToRecipeCommand;
 import com.maartenmusic.recipeproject.domain.Recipe;
@@ -51,6 +52,27 @@ public class RecipeDBServiceTest {
         verify(recipeRepository, times((1))).findById(anyLong());
         verify(recipeRepository, never()).findAll();
     }
+
+    @Test
+    public void getRecipeCommandByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull("Null recipe returned", commandById);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
 
     @Test
     public void findAllTest() {
